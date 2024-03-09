5d7b342eb90a4f87379151bfa7a71e5715569d0d
IDOMATH <btdothemath@gmail.com>
2024-03-08 20:57:02 -0600

    Adding image location to recipe instructions

--- types/recipe.go
+++ types/recipe.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Recipe struct {
 	Id           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Ingredients  []Ingredient       `bson:"ingredients" json:"ingredients"`
-	Instructions []string           `bson:"instructions" json:"instructions"`
+	Instructions []Instruction      `bson:"instructions" json:"instructions"`
 	Author       string             `bson:"author" json:"author"`
 }
 
@@ -13,4 +13,9 @@ type Ingredient struct {
 	Name     string
 	Quantity string // Want this to be a string for thing like fractions
 	Unit     string
+}
+
+type Instruction struct {
+	ImageLocation string
+	Text          string
 }
