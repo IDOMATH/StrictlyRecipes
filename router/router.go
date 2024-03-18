079a1ecc71674777e7ccd84ed1c8064e6d0058b8
IDOMATH <btdothemath@gmail.com>
2024-03-17 21:19:33 -0500

    Adding regex to routeRecipes

--- router/router.go
+++ router/router.go
@@ -4,8 +4,11 @@ import (
 	"github.com/IDOMATH/StrictlyRecipes/render"
 	"github.com/IDOMATH/StrictlyRecipes/types"
 	"net/http"
+	"regexp"
 	"strings"
 )
+
+var regexNumber = regexp.MustCompile(`\d`)
 
 type Router struct {
 	urlIndex int
@@ -31,12 +34,11 @@ func (rtr *Router) Route(w http.ResponseWriter, r *http.Request) {
 func (rtr *Router) routeRecipes(w http.ResponseWriter, r *http.Request) {
 	rtr.urlIndex++
 	url := strings.Split(r.URL.Path, "/")
-	switch url[rtr.urlIndex] {
-	case "":
-		render.Template(w, r, "all-recipes.go.html", &types.TemplateData{PageTitle: "All Recipes"})
-	case ":id":
+	switch {
+	case regexNumber.MatchString(url[rtr.urlIndex]):
 		// TODO: handle get recipe by ID and do the regex on case
 	}
+	render.Template(w, r, "all-recipes.go.html", &types.TemplateData{PageTitle: "All Recipes"})
 
 }
 
