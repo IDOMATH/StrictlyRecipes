8b888afa24c02976ad1b2d1d2d742e84be2f8369
IDOMATH <btdothemath@gmail.com>
2024-04-04 20:32:05 -0500

    Adding middleware stack

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,6 +6,19 @@ import (
 	"time"
 )
 
+type Middleware func(handler http.Handler) http.Handler
+
+func CreateStack(stack ...Middleware) Middleware {
+	return func(next http.Handler) http.Handler {
+		for i := len(stack) - 1; i >= 0; i-- {
+			mw := stack[i]
+			next = mw(next)
+		}
+
+		return next
+	}
+}
+
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
